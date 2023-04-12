bba9909e020ec53c6750a3b7cebe71618bf28ca1
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-12 14:15:21 -0500

    Create main.java

---
+++ spinner/main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Spinner Game! Is your luck better than a computer's? (Probably not)");
+    SpinnerGame myGame = new SpinnerGame();
+    myGame.playRound();
+  }
+}
